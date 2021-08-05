25de1ba0a82eb015fefc3ebf3430cc2a0c175a1b
Yanghony <17697012823@163.com>
2021-08-05 15:02:33 +0800

    feat(公司模块): 创建公司模块实体层，服务层，数据层，控制层

---
+++ src/main/java/com/kayak/myobject/controller/CompanyController.java
@@ -0,0 +1,12 @@
+package com.kayak.myobject.controller;
+
+/**
+ * @author yangyy
+ * @version 1.0
+ * @date 2021/8/5 10:53
+ * @projectName:
+ * @Description: 公司控制层
+ */
+public class CompanyController {
+
+}
